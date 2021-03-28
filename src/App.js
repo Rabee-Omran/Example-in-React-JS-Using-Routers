6828f2b4e222b89380372ecf1be14f3b945f1f46
RabeeOmran-dev <rabeeomran.dev@gmail.com>
2021-03-28 23:12:57 +0300

    add home and about page routers

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Route , Link } from "react-router-dom";
 
-class App extends Component{
 
+const HomePage = () => <div>Home</div>
+const AboutPage = () => <div>About</div>
+
+
+class App extends Component{  
 
   render(){
 
      
     return (
-      
+    <BrowserRouter>
+
       <div>
+        <Link to ='/'>Home</Link> {"  "}
+        <Link to ='/about'>About</Link>
+
+        <Route path= "/" exact component={HomePage} />
+        <Route path= "/about"  component={AboutPage} />
 
 
           <h1>TEST</h1>  
 
 
       </div>
+
+    </BrowserRouter>
     );
   }
 }
