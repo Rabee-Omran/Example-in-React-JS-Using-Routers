8c7af7997ea33d415b5ad414a27a5d9215311f6a
RabeeOmran-dev <rabeeomran.dev@gmail.com>
2021-03-28 22:59:31 +0300

    initial files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+
+
+  render(){
+
+     
+    return (
+      
+      <div>
+
+
+          <h1>TEST</h1>  
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
